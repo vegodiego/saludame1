f289178eb6420e488635d278d6256c4c9b188560
vegodiego <53877309+vegodiego@users.noreply.github.com>
2019-10-21 21:21:18 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -2,7 +2,11 @@ require "sinatra"
 
 get "/" do
     if params[:nombre]
-        "<h1>Hola #{params[:nombre]}!</h1>"
+        if params[:nombre]=="" 
+            "<h1>Hola desconocido!</h1>"
+        else
+            "<h1>Hola #{params[:nombre]}!</h1>"   
+        end
     else
         "<h1>Hola desconocido!</h1>"
     end
